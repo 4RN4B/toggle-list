a1ee53ad0c3bcc19ecdde29c09fb22c65e83e670
Arnab Mondal <arnabjuniormondal123@gmail.com>
2022-04-05 00:07:30 +0530

    feat: completed the list delete button operation

--- client/index.js
+++ client/index.js
@@ -187,24 +187,38 @@ const handleListButtons = (e, element) => {
             skillColumn.innerHTML = previousData;
         }
     } else {
-        handleFetch("/", {
-            onSuccess: (data) => {
-                skillColumn.innerHTML = data[id - 1].skills;
-            },
-        });
-        let index;
-        for (let i = 0; i < e.path.length; i++) {
-            let classes = e.path[i].classList;
-            if (classes.contains("table-list-buttons")) {
-                index = i;
-                break;
+        let previousButtonClass = document
+            .getElementById(`list-${id}`)
+            .querySelector(".table-list-buttons").children[0];
+        if (previousButtonClass.classList.contains("edit-button")) {
+            handleFetch("/" + id, {
+                method: "DELETE",
+                onSuccess: (data) => {
+                    window.location.reload();
+                },
+                data: {},
+            });
+        } else {
+            //!Check here error when edit and cancel is pressed
+            handleFetch("/", {
+                onSuccess: (data) => {
+                    skillColumn.innerHTML = data[id - 1].skills;
+                },
+            });
+            let index;
+            for (let i = 0; i < e.path.length; i++) {
+                let classes = e.path[i].classList;
+                if (classes.contains("table-list-buttons")) {
+                    index = i;
+                    break;
+                }
             }
-        }
-        let previousButton = e.path[index].firstElementChild;
-        if (previousButton.classList.contains("save-button")) {
-            previousButton.classList.remove("save-button");
-            previousButton.classList.add("edit-button");
-            previousButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"> <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" /> <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" /> </svg>`;
+            let previousButton = e.path[index].firstElementChild;
+            if (previousButton.classList.contains("save-button")) {
+                previousButton.classList.remove("save-button");
+                previousButton.classList.add("edit-button");
+                previousButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"> <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" /> <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" /> </svg>`;
+            }
         }
     }
 };
@@ -224,7 +238,6 @@ const handleCardButtons = (e, element) => {
         handleFetch("/" + id, {
             method: "DELETE",
             onSuccess: (data) => {
-                // console.log(data);
                 window.location.reload();
             },
             data: {},
@@ -237,6 +250,7 @@ const handleCardButtons = (e, element) => {
                 previousData = skillElement;
                 skillElement.innerHTML = `<span>Skills: </span><input type="text" id="skills-info-${id}" class="skills" name="skills"></input>`;
             } else {
+                // TODO: If inputted nothing after clicking Edit and then Saved previous data will be restored.
                 element.innerHTML = "Edit";
                 let newSkill = document.getElementById(`skills-info-${id}`);
                 if (newSkill.value !== "") {
@@ -257,6 +271,7 @@ const handleCardButtons = (e, element) => {
             if (previousButton.innerHTML.trim() === "Save") {
                 previousButton.innerHTML = "Edit";
             }
+            //!Check here error when edit and cancel is pressed
             handleFetch("/", {
                 onSuccess: (data) => {
                     skillElement.innerHTML = `<div class="skills">Skills: ${
