57b3b5a5987827ddacdf8c9e4045c5ef92b56702
Arnab Mondal <arnabjuniormondal123@gmail.com>
2022-04-04 23:53:27 +0530

    feat: implemented card delete button and fixed some code

--- client/index.js
+++ client/index.js
@@ -2,7 +2,8 @@ const serverAddress = "http://localhost:3000";
 
 const createCard = (user) => {
     let temp = document.createElement("div");
-    temp.id = `${user.id}`;
+    temp.id = `div-${user.id}`;
+    temp.setAttribute("data-card-id", user.id);
     temp.className = "card-slides m-2 p-10 relative";
     temp.innerHTML = `<div class="card-image p-1 ">
         <img class="image-fit" src=${
@@ -12,8 +13,9 @@ const createCard = (user) => {
         } alt="">
     </div>
     <div class="card-body">
+    <div class="name f-1 fw-600 p-10">ID: ${user.id}</div>
         <div class="name f-1 fw-600 p-10">Name: ${user.name}</div>
-        <div class="id p-10">ID: ${user.emp_id}</div>
+        <div class="id p-10">Employee-ID: ${user.emp_id}</div>
         <div class="skills p-10">Skills: ${user.skills}</div>
         <div class="project p-10">Project: ${user.project}</div>
         <div class="hcm p-10">HCM: ${user.hcm}</div>
@@ -27,7 +29,7 @@ const createCard = (user) => {
         </button>
     </div>
     <div class="close absolute">
-        <button class="close-button">
+        <button class="close-button card-action-button">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
                 class="bi bi-x-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
@@ -41,9 +43,11 @@ const createCard = (user) => {
 
 const createList = (user) => {
     let temp = document.createElement("tr");
-    temp.id = `${user.id}`;
+    temp.id = `list-${user.id}`;
+    temp.setAttribute("data-list-id", user.id);
     temp.className = "table-row";
-    temp.innerHTML = `<td>${user.name}</td>
+    temp.innerHTML = `<td>${user.id}</td>
+    <td>${user.name}</td>
     <td>${user.emp_id}</td>
     <td class="skills">
         <span class="display-inline-block">${user.skills}</span>
@@ -92,31 +96,33 @@ const handleFetch = (location, { method = "GET", onSuccess, data = {} }) => {
         });
 };
 
-handleFetch("/", {
-    onSuccess: (data) => {
-        const gridView = document.getElementById("cards-section");
-        const listView = document.querySelector("#list-section table");
-        data.forEach((element) => {
-            gridView.appendChild(createCard(element));
-            listView.appendChild(createList(element));
-        });
-        const listButtons = [...document.querySelectorAll(".list-buttons")];
-        listButtons.forEach((element) => {
-            element.addEventListener("click", (e) => {
-                handleListButtons(e, element);
+const temporary = () => {
+    handleFetch("/", {
+        onSuccess: (data) => {
+            const gridView = document.getElementById("cards-section");
+            const listView = document.querySelector("#list-section table");
+            data.forEach((element) => {
+                gridView.appendChild(createCard(element));
+                listView.appendChild(createList(element));
             });
-        });
-        const cardButtons = [
-            ...document.querySelectorAll(".card-action-button"),
-        ];
-        cardButtons.forEach((element) => {
-            element.addEventListener("click", (e) => {
-                handleCardButtons(e, element);
+            const listButtons = [...document.querySelectorAll(".list-buttons")];
+            listButtons.forEach((element) => {
+                element.addEventListener("click", (e) => {
+                    handleListButtons(e, element);
+                });
             });
-        });
-    },
-});
-
+            const cardButtons = [
+                ...document.querySelectorAll(".card-action-button"),
+            ];
+            cardButtons.forEach((element) => {
+                element.addEventListener("click", (e) => {
+                    handleCardButtons(e, element);
+                });
+            });
+        },
+    });
+};
+temporary();
 /**
  * For toggling the grid and list icon
  */
@@ -148,44 +154,38 @@ document.getElementById("toggle-button").addEventListener("click", (e) => {
     }
 });
 
+let previousData;
 const handleListButtons = (e, element) => {
-    let index;
-    for (let i = 0; i < e.path.length; i++) {
-        let classes = e.path[i].classList;
-        if (classes.contains("table-row")) {
-            index = i;
-            break;
-        }
-    }
-    let skillColumn = e.path[index].childNodes[4];
-    let id = e.path[index].id;
-    let cardSkills =
-        document.getElementById("cards-section").children[id - 1].children[1]
-            .children[2];
+    let id = Number(e.path[2].getAttribute("data-list-id"));
+    let skillColumn = document
+        .getElementById(`list-${id}`)
+        .querySelector(".skills");
+    let cardSkills = document
+        .getElementById(`div-${id}`)
+        .querySelector(".skills");
     if (element.classList.contains("edit-button")) {
         element.classList.remove("edit-button");
         element.classList.add("save-button");
         element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-save" viewBox="0 0 16 16"> <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z" /> </svg>`;
-        skillColumn.innerHTML = `<input type="text" id="skills-info" class="skills" name="skills"></input>`;
+        previousData = skillColumn.innerHTML;
+        skillColumn.innerHTML = `<input type="text" id="skills-info-${id}" class="skills" name="skills"></input>`;
     } else if (element.classList.contains("save-button")) {
         element.classList.remove("save-button");
         element.classList.add("edit-button");
         element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16"> <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" /> <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" /> </svg>`;
-        let newSkill = document.getElementById("skills-info");
+        let newSkill = document.getElementById(`skills-info-${id}`);
         if (newSkill.value !== "") {
-            console.log(newSkill.value);
             handleFetch("/" + id, {
                 method: "PATCH",
-                onSuccess: (data) => {},
+                onSuccess: (data) => {
+                    skillColumn.innerHTML = newSkill.value;
+                    cardSkills.innerHTML = `Skills: ${newSkill.value}`;
+                },
                 data: { skills: newSkill.value },
             });
+        } else {
+            skillColumn.innerHTML = previousData;
         }
-        handleFetch("/", {
-            onSuccess: (data) => {
-                skillColumn.innerHTML = data[id - 1].skills;
-                cardSkills.innerHTML = `Skills: ${data[id - 1].skills}`;
-            },
-        });
     } else {
         handleFetch("/", {
             onSuccess: (data) => {
@@ -210,45 +210,61 @@ const handleListButtons = (e, element) => {
 };
 
 const handleCardButtons = (e, element) => {
-    let skillElement = e.path[2].childNodes[2].children[2];
-    let id = e.path[2].id;
-    let listSkill =
-        document.getElementById("list-section").children[0].children[0]
-            .children[id].children[2].children[0];
-    if (element.classList.contains("save-edit-button")) {
-        if (element.innerHTML.trim() === "Edit") {
-            element.innerHTML = "Save";
-            skillElement.innerHTML = `<span>Skills: </span><input type="text" id="skills-info" class="skills" name="skills"></input>`;
+    // console.log(e);
+    let id = Number(e.path[2].getAttribute("data-card-id"));
+    let listSkill = document
+        .getElementById(`list-${id}`)
+        .querySelector(".skills");
+    let skillElement = document
+        .getElementById(`div-${id}`)
+        .querySelector(".skills");
+    previousData = skillElement.innerHTML;
+    // console.log(skillElement);
+    if (element.classList.contains("close-button")) {
+        handleFetch("/" + id, {
+            method: "DELETE",
+            onSuccess: (data) => {
+                // console.log(data);
+                window.location.reload();
+            },
+            data: {},
+        });
+    } else {
+        listSkill = listSkill.children[0];
+        if (element.classList.contains("save-edit-button")) {
+            if (element.innerHTML.trim() === "Edit") {
+                element.innerHTML = "Save";
+                previousData = skillElement;
+                skillElement.innerHTML = `<span>Skills: </span><input type="text" id="skills-info-${id}" class="skills" name="skills"></input>`;
+            } else {
+                element.innerHTML = "Edit";
+                let newSkill = document.getElementById(`skills-info-${id}`);
+                if (newSkill.value !== "") {
+                    handleFetch("/" + id, {
+                        method: "PATCH",
+                        onSuccess: (data) => {
+                            skillElement.innerHTML = `<div class="skills p-10">Skills: ${newSkill.value}</div>`;
+                            listSkill.innerHTML = newSkill.value;
+                        },
+                        data: { skills: newSkill.value },
+                    });
+                } else {
+                    skillElement.innerHTML = `${previousData}`;
+                }
+            }
         } else {
-            let newSkill = document.getElementById("skills-info");
-            if (newSkill.value !== "") {
-                handleFetch("/" + id, {
-                    method: "PATCH",
-                    onSuccess: (data) => {},
-                    data: { skills: newSkill.value },
-                });
+            let previousButton = e.path[2].children[2].firstElementChild;
+            if (previousButton.innerHTML.trim() === "Save") {
+                previousButton.innerHTML = "Edit";
             }
             handleFetch("/", {
                 onSuccess: (data) => {
                     skillElement.innerHTML = `<div class="skills">Skills: ${
                         data[id - 1].skills
                     }</div>`;
-                    listSkill.innerHTML = data[id - 1].skills;
                 },
             });
         }
-    } else {
-        let previousButton = e.path[2].children[2].firstElementChild;
-        if (previousButton.innerHTML.trim() === "Save") {
-            previousButton.innerHTML = "Edit";
-        }
-        handleFetch("/", {
-            onSuccess: (data) => {
-                skillElement.innerHTML = `<div class="skills">Skills: ${
-                    data[id - 1].skills
-                }</div>`;
-            },
-        });
     }
 };
 
